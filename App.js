f4986f23949e7555fad84d62f2b260239b75715e
ericmlantz <ericmlantz@gmail.com>
2022-08-08 15:52:10 -0400

    Made Default Containers for React Native Repo

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//imported 3rd party libraries
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 
+//imported from this repo
+import ComponentExamples from './app/screens/ComponentExamples'
+import Screen from './app/components/Screen'
+
+//make this component available to view
 export default function App() {
+  //render
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Screen style={styles.container}>
+
+      <ComponentExamples />
+      {/* ComponentExamples is found in ./app/screens/ComponentExamples.js */}
+
       <StatusBar style="auto" />
-    </View>
-  );
+      {/* Sets the color of the status bar text. Default value is "auto" which picks the appropriate value according to the active color scheme, eg: if your app is dark mode, the style will be "light". */}
+
+    </Screen>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff', //White Background
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
